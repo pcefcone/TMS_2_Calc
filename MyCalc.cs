55fc1361c1d09a2db64aa52ee22da75795885a3b
Yaroslav <yaroslav.kerez@gmail.com>
2024-06-27 20:01:20 +0200

    Add project files.

---
+++ MyCalc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator
+{
+    
+    public class MyCalc()
+    {          
+        public static double AddOperation(double firstInput, double secondInput)
+        {
+            return firstInput + secondInput;
+        }
+        public static double SubOperation(double firstInput, double secondInput)
+        {
+            return firstInput - secondInput;
+        }
+        public static double SquareOperation(double firstInput, double secondInput)
+        {
+            return Math.Pow(firstInput, secondInput);
+        }
+        public static double MultiplicationOperation(double firstInput, double secondInput)
+        {
+            return  firstInput * secondInput;
+        }
+        public static double DivisionOperation(double firstInput, double secondInput)
+        {
+            return firstInput / secondInput;
+        }
+        public static double ModuleOperation(double firstInput, double secondInput)
+        {
+            return firstInput % secondInput;
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using Calculator;
+
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCalc calc = new MyCalc();
+
+
+            while (true)
+            {
+            Console.WriteLine("Put first number: ");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Put second number: ");
+            string secondInput = Console.ReadLine();
+
+                if (double.TryParse(firstInput, out double firstNumber) && double.TryParse(secondInput, out double secondNumber))
+                {
+                    Console.WriteLine("Please choose the following you want to do");
+                    Console.WriteLine("Choose '+' to calculate the addition");
+                    Console.WriteLine("Choose '-' to calculate the substraction");
+                    Console.WriteLine("Choose '*' to calculate the multiplication");
+                    Console.WriteLine("Choose '/' to calculate the division");
+                    Console.WriteLine("Choose '^' to calculate the exponentiation");
+                    Console.WriteLine("Choose '%' to calculate the module");
+                    Console.WriteLine("Your choose is: ");
+
+                    switch (Console.ReadLine())
+                    {
+                        // addition
+                        case "+":
+                            Console.WriteLine(value: $"Your result is: " +
+                                $"{MyCalc.AddOperation(firstNumber, secondNumber)}");
+                            break;
+
+                        // substraction
+                        case "-":
+                            Console.WriteLine(value: $"Your result is: " +
+                                $"{MyCalc.SubOperation(firstNumber, secondNumber)}"); ;
+                            break;
+
+                        // multiplication
+                        case "*":
+                            Console.WriteLine(value: $"Your result is: " +
+                                $"{MyCalc.MultiplicationOperation(firstNumber, secondNumber)}");
+                            break;
+
+                        // division
+                        case "/":
+                            Console.WriteLine(value: $"Your result is: " +
+                                $"{MyCalc.DivisionOperation(firstNumber, secondNumber)}");
+                            break;
+
+                        // square
+                        case "^":
+                            Console.WriteLine(value: $"Your result is: " +
+                                $"{MyCalc.SquareOperation(firstNumber, secondNumber)}");
+                            break;
+
+                        // square
+                        case "%":
+                            Console.WriteLine(value: $"Your result is: " +
+                                $"{MyCalc.ModuleOperation(firstNumber, secondNumber)}");
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown operaion, please try again");
+                            break;
+                    }
+                    break;
+                }
+                Console.WriteLine("Input data must be numbers");
+            }
+
+
+
+
+
+
+
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+
